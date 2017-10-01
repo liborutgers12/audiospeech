b9fbf7d38a66d17207dddb27e96b8f90825a6dbf
libo <boli.pku@gmail.com>
2017-09-30 22:21:16 -0700

    added readPlotWave.py

---
+++ readPlotWave.py
@@ -0,0 +1,12 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+sampling_freq, audio = wavfile.read('input.wav')
+
+print '\nShape:', audio.shape
+print 'Datatype:', audio.dtype
+print 'Duration:', round(audio.shape[0] / float(sampling_freq), 3), 'seconds'
+
+# Normalize the audio
+audio = audio / (2.**15)
