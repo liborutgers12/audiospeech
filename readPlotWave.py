e7b826aab59e8f95cc37f8c26e3b3014604dc22b
libo <boli.pku@gmail.com>
2017-09-30 22:28:39 -0700

    small changes to readPlotWave.py

--- readPlotWave.py
+++ readPlotWave.py
@@ -10,3 +10,12 @@ print 'Duration:', round(audio.shape[0] / float(sampling_freq), 3), 'seconds'
 
 # Normalize the audio
 audio = audio / (2.**15)
+
+# audio = audio[:30]
+x_values = np.arange(0, len(audio), 1) / float(sampling_freq)
+x_values = x_values * 1000
+plt.plot(x_values, audio, color='black')
+plt.xlabel('Time (ms)')
+plt.ylabel('Amplitude')
+plt.title('Audio signal')
+plt.show()
