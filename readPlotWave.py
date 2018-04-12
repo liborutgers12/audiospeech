181cf5489635735403c0a90b46a6454ea7189f67
Bo Li <boli.pku@gmail.com>
2018-04-12 00:17:38 -0700

    Refactoring...

--- readPlotWave.py
+++ readPlotWave.py
@@ -1,92 +1,25 @@
-﻿import numpy as np
+﻿from __future__ import division 
+from __future__ import print_function
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+import preprocessing
+import visualization
 
 samplingFreq, audio = wavfile.read('input.wav')
 
-print('\nShape:', audio.shape)
+print(max(audio))
+print('Shape:', audio.shape)
 print('Datatype:', audio.dtype)
 print('SamplingFrequency:', samplingFreq)
-print('Duration:', round(audio.shape[0] / float(samplingFreq), 3), 'seconds')
+print('Duration:', round(audio.shape[0] / samplingFreq, 3), 'seconds')
 
 # Normalize the audio
-audio = audio / (2.**15)
+audio = audio / max(abs(audio))
 
 # Plot the audio in time domain
-# audio = audio[:30]
-x_values = np.arange(0, len(audio), 1) / float(samplingFreq)
-x_values = x_values * 1000
-plt.plot(x_values, audio, color='black')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-plt.title('Audio signal')
-#plt.show()
-
-def audioPowerSpectr(audio, samplingFreq, plotEnabled):
-	lenAudio = len(audio)
-	# Apply Fourier transform
-	transformedSignal = np.fft.fft(audio)
-	halfLength = np.ceil((lenAudio + 1) / 2.0)
-	transformedSignal = abs(transformedSignal[0:halfLength])
-	transformedSignal /= float(lenAudio)
-	transformedSignal **= 2
-
-	lenTransformed = len(transformedSignal)
-	if lenAudio % 2:
-		transformedSignal[1:lenTransformed] *= 2
-	else:
-		transformedSignal[1:lenTransformed] *= 2
-
-	# Express the power spectrum in dB
-	power = 10 * np.log10(transformedSignal)
-
-	# Plot the audio power spectrum
-	if plotEnabled:
-		x_values = np.arange(0, halfLength, 1) * (samplingFreq / lenAudio) / 1000.0
-		plt.figure()
-		plt.plot(x_values, power, color='black')
-		plt.xlabel('Freq (in kHz)')
-		plt.ylabel('Power (in dB)')
-		plt.show()
-	return power
-
-
-def audioSpectrogram(audio, samplingFreq, fftLength=512, overlapRatio=0.5, plotEnabled=True):
-	# Get the power spectrum for segments of the audio
-	numSegs = np.floor((len(audio)-fftLength)/(fftLength*overlapRatio)) + 1
-	audioSpectrogram = np.zeros((fftLength/2 + 1, numSegs))
-	for idx in np.arange(0, len(audio) - fftLength, fftLength * overlapRatio):
-		audioSeg = audio[idx:idx+fftLength]
-		audioSpectrogram[:,int(idx / (fftLength * overlapRatio))] = audioPowerSpectr	(audioSeg, samplingFreq, False)
-
-	from mpl_toolkits.mplot3d import Axes3D
-	import matplotlib.cm as cm
-
-	t = np.arange(0, len(audio) - fftLength, fftLength * overlapRatio) / samplingFreq
-	f = np.arange(0, fftLength/2 + 1) / float(fftLength) * samplingFreq / 1000.0
-	tGrid, fGrid = np.meshgrid(t,f)
-
-	if False:
-		fig = plt.figure(1)
-		ax = fig.add_subplot(111, projection='3d')
-		ax.plot_surface(tGrid,fGrid, np.abs(audioSpectrogram), cmap=cm.jet, linewidth=0)
-		ax.view_init(azim=-90, elev=90)
-
-		fig = plt.figure(2)
-		ax2 = fig.add_subplot(111)
-		ax2.specgram(audio)
-		ax2.axis('tight')
-
-	if plotEnabled:
-		fig = plt.figure()
-		ax3 = fig.add_subplot(111)
-		ax3.pcolormesh(tGrid,fGrid, 10*np.log10(np.abs(audioSpectrogram)), cmap=cm.jet)
-		ax3.axis('tight')
-		ax3.set_xlabel('Time(sec)')
-		ax3.set_ylabel('Frequency(kHz)')
-
-		plt.show()
+visualization.plotWaveform(audio, samplingFreq)
 
 fftLength = 512
 overlapRatio = 0.5
-audioSpectrogram(audio,samplingFreq,fftLength,overlapRatio,True)
+preprocessing.audioSpectrogram(audio,samplingFreq,fftLength,overlapRatio,True)
