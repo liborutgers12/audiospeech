78d5342ebea4f3825b353407c0fa69aa2601429b
Bo Li <boli.pku@gmail.com>
2018-04-12 22:27:40 -0700

    Some refactor.

--- readPlotWave.py
+++ readPlotWave.py
@@ -4,22 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import preprocessing
-import visualization
+import utils
+import sys
 
-samplingFreq, audio = wavfile.read('input.wav')
+if len(sys.argv) < 2:
+	fileName = '/home/boli/Documents/audiospeech/records.wav'
+else:
+	fileName = sys.argv[1]
+samplingFreq, audio = wavfile.read(fileName)
 
-print(max(audio))
+print(np.amax(audio))
 print('Shape:', audio.shape)
 print('Datatype:', audio.dtype)
 print('SamplingFrequency:', samplingFreq)
 print('Duration:', round(audio.shape[0] / samplingFreq, 3), 'seconds')
 
 # Normalize the audio
-audio = audio / max(abs(audio))
+audio = audio / np.amax(np.abs(audio))
 
 # Plot the audio in time domain
-visualization.plotWaveform(audio, samplingFreq)
+utils.plotWaveforms(audio, samplingFreq)
+utils.playBack(audio, samplingFreq)
 
 fftLength = 512
 overlapRatio = 0.5
-preprocessing.audioSpectrogram(audio,samplingFreq,fftLength,overlapRatio,True)
+if audio.ndim > 1:
+	audio1CH = audio[:,0]
+else:
+	audio1CH = audio
+preprocessing.audioSpectrogram(audio1CH,samplingFreq,fftLength,overlapRatio,True)
